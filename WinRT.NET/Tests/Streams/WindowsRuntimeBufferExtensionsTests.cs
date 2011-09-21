940d779b1b45a97dbafa576fea004ac65967852e
ermau <me@ermau.com>
2011-09-20 21:22:50 -0400

    Oops, missed the files

---
+++ WinRT.NET/Tests/Streams/WindowsRuntimeBufferExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿//
+// WindowsRuntimeBufferExtensionsTests.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2011 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework;
+using Windows.Storage.Streams;
+
+namespace WinRTNET.Tests.Streams
+{
+	[TestFixture]
+	public class WindowsRuntimeBufferExtensionsTests
+	{
+		[Test]
+		public void AsBuffer_Null()
+		{
+			Assert.Throws<ArgumentNullException> (() => WindowsRuntimeBufferExtensions.AsBuffer (null));
+		}
+
+		[Test]
+		public void AsBuffer()
+		{
+			byte[] array = new byte[5];
+			IBuffer buffer = array.AsBuffer();
+
+			Assert.IsNotNull (buffer);
+			Assert.AreEqual (array.Length, buffer.Length);
+			Assert.AreEqual (array.Length, buffer.Capacity);
+		}
+	}
+}
---
+++ WinRT.NET/Tests/Streams/WindowsRuntimeBufferTests.cs
@@ -0,0 +1,73 @@
+﻿//
+// WindowsRuntimeBufferTests.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2011 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using NUnit.Framework;
+
+namespace WinRTNET.Tests.Streams
+{
+	[TestFixture]
+	public class WindowsRuntimeBufferTests
+	{
+		[Test]
+		public void Capacity()
+		{
+			byte[] array = new byte[5];
+			var buffer = new WindowsRuntimeBuffer (array);
+
+			Assert.AreEqual (array.Length, buffer.Capacity);
+		}
+
+		[Test]
+		public void Length_Get()
+		{
+			byte[] array = new byte[5];
+			var buffer = new WindowsRuntimeBuffer (array);
+
+			Assert.AreEqual (array.Length, buffer.Length);
+		}
+
+		[Test]
+		public void Length_Set()
+		{
+			byte[] array = new byte[5];
+			var buffer = new WindowsRuntimeBuffer (array);
+
+			buffer.Length = 4;
+			Assert.AreEqual (4, buffer.Length);
+		}
+
+		[Test]
+		public void Length_OutOfRange()
+		{
+			byte[] array = new byte[5];
+			var buffer = new WindowsRuntimeBuffer (array);
+
+			Assert.Throws<ArgumentOutOfRangeException> (() => buffer.Length = 6);
+		}
+	}
+}
