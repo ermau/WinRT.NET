03ab1cd9940c3d127708199c56d06a4f7dac2bb1
ermau <me@ermau.com>
2011-09-26 19:06:11 -0400

    Stream contract adapter stubs

---
+++ WinRT.NET/System/IO/OutputStreamToStreamAdapter.cs
@@ -0,0 +1,106 @@
+﻿//
+// OutputStreamToStreamAdapter.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2011 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Linq;
+using Windows.Storage.Streams;
+
+namespace Windows.System.IO
+{
+	internal class OutputStreamToStreamAdapter
+		: Stream
+	{
+		public OutputStreamToStreamAdapter(IOutputStream stream)
+		{
+			this.stream = stream;
+		}
+
+		internal IOutputStream Stream
+		{
+			get { return this.stream; }
+		}
+
+		public override bool CanRead
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override bool CanSeek
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override bool CanWrite
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override void Flush()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override long Length
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override long Position
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override int Read (byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override long Seek (long offset, SeekOrigin origin)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetLength (long value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Write (byte[] buffer, int offset, int count)
+		{
+			throw new NotImplementedException();
+		}
+
+		private readonly IOutputStream stream;
+	}
+}
