5f822ad56abc96a838938c3e8569261f9de95835
ermau <me@ermau.com>
2011-09-28 18:44:06 -0400

    Crypto hash stubs

---
+++ WinRT.NET/Security/Cryptography/Core/HashAlgorithmProvider.cs
@@ -0,0 +1,64 @@
+﻿//
+// HashAlgorithmProvider.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2011 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+
+namespace Windows.Security.Cryptography.Core
+{
+	public sealed class HashAlgorithmProvider
+	{
+		internal HashAlgorithmProvider()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string AlgorithmName
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public uint HashLength
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public CryptographicHash CreateHash()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static IReadOnlyList<string> EnumerateAlgorithms()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static HashAlgorithmProvider OpenAlgorithm (string algorithm)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
